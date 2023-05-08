620004f8a68bde6255f38605d300e1d615960834
kamarini <kama0549@stud.kea.dk>
2023-05-08 17:09:29 +0200

    not done complete

---
+++ src/components/Layout/index.js
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router';
+import Anchor from '../Anchor';
+import styles from './Layout.module.css';
+
+export default function Layout({ children, navData }) {
+  const router = useRouter();
+
+  return (
+    <div className='wrapper'>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          {navData.map((item, index) => (
+            <li className={styles.li} key={index}>
+              <Anchor
+                href={item.link}
+                className={`${styles.a} ${
+                  router.asPath === item.link ? styles.active : ''
+                }`}
+              >
+                {item.name}
+              </Anchor>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>{children}</main>
+      <footer> Kamarini Moragianni</footer>
+    </div>
+  );
+}
